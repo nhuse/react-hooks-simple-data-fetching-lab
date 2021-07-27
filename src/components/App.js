7afffe2a90967552ca598a7cd7000d1df64e102a
nhuse <nathan.huse@yahoo.com>
2021-07-26 22:54:05 -0500

    commit tree 7009a8d81065ebf24f144abbaa30e918fc24e8f9

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
+import { useEffect, useState } from "react"
 // create your App component here
+function App() {
+    const [img, setImg] = useState(null)
+    function handleImg(imgSrc){
+        setImg(imgSrc)
+    }
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(resp => resp.json())
+        .then(json => handleImg(json.message))
+    }, [])
+
+    return (
+        <div>
+            {img ? <img src={img} alt="A Random Dog"/> : <p>Loading...</p>}
+        </div>
+    )
+}
+
+export default App;
